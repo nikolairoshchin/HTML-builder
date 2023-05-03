14c2009a704b21441de72b5c3a6fb3abc9954ca8
nikolairoshchin <99800316+nikolairoshchin@users.noreply.github.com>
2023-05-03 22:12:47 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
+const path = require("path");
 
+const source = path.join(__dirname, "files");
+const destination = path.join(__dirname, "files-copy");
+
+fs.mkdir(destination, { recursive: true }, (err) => {
+  if (err) return console.error(err);
+  fs.readdir(source,
+	        { withFileTypes: true },
+	        (err, files) => {
+              for (const file of files) {
+  	            fs.copyFile(path.join(source, file.name),
+  	            	        path.join(destination, file.name),
+  	            	        (err) => {
+  	                          if (err) return console.error(err);
+  	                        });
+              }
+  });
+  console.log('Directory created successfully!');
+});
